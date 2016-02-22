882dd9b048de673bb0f82dfcbcf9a77b17ca0027
Michael Sebastian <michael@urbansitter.com>
2016-02-22 09:10:16 -0800

    Flyway migration on startup

--- src/main/java/com/linguo/Application.java
+++ src/main/java/com/linguo/Application.java
@@ -1,18 +1,22 @@
 package com.linguo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.data.repository.support.DomainClassConverter;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.flywaydb.core.Flyway;
 
-@SpringBootApplication
 @EnableWebMvc
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+
     public static void main(String[] args) throws Throwable {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(System.getenv("BOXFUSE_DATABASE_URL"),
+                System.getenv("BOXFUSE_DATABASE_USER"),
+                System.getenv("BOXFUSE_DATABASE_PASSWORD"));
+        flyway.migrate();
         SpringApplication.run(Application.class, args);
     }
 }
